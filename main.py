2d98decdae4bc292c4aaffa8bb24019e316b8b15
BluesJiang <763400095@qq.com>
2017-06-01 16:32:32 +0800

    comma

--- main.py
+++ main.py
@@ -144,10 +144,10 @@ def main():
           )
     w.pack()
     for circle in circles:
-        point1 = circle.center[0] - circle.radius;
-        point2 = circle.center[1] - circle.radius;
-        point3 = circle.center[0] + circle.radius;
-        point4 = circle.center[1] + circle.radius;
+        point1 = circle.center[0] - circle.radius
+        point2 = circle.center[1] - circle.radius
+        point3 = circle.center[0] + circle.radius
+        point4 = circle.center[1] + circle.radius
         w.create_oval(400 + 300 * point1, 400 + 300 * point2, 400 + 300 * point3, 400 + 300 * point4, fill = "gray");
  
     mainloop()
