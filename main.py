017a074fc3b1b28dc07fd1314ae2fb13aa485ecd
BluesJiang <763400095@qq.com>
2017-06-01 10:30:55 +0800

    clear the formula

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ class Circle():
         self.radius = radius
     
     def description(self):
-        return ("Center: (%.2e, %.2e)\tRadius:%.2e"%(self.center[0], self.center[1], self.radius))
+        return ("Center: (%f, %f)\tRadius:%f"%(self.center[0], self.center[1], self.radius))
 
 
 def sub_solution_r(m):
     circles = []
+    R = [1]
     circles.append(Circle((0, 0), 1))
     sym_x = [1, 1, -1, -1]
     sym_y = [1, -1, 1, -1]
@@ -24,33 +25,34 @@ def sub_solution_r(m):
     elif m <= 5:
         r = 3 - 2 * math.sqrt(2)
         y = x = 1 - r
+        R.append(r)
         for i in range(0, 4):
             circles.append(Circle((x * sym_x[i], y * sym_y[i]), r))
         return circles
     elif m > 5:
-        r0 = 3 - 2 * math.sqrt(2)
-        radius = [r0]
-        y = x = 1 - r0
+        R1 = 3 - 2 * math.sqrt(2)
+        R.append(R1)
+        y = x = 1 - R1
         for i in range(0, 4):
-            circles.append(Circle((x * sym_x[i], y * sym_y[i]), r0))
+            circles.append(Circle((x * sym_x[i], y * sym_y[i]), R1))
         pend_height = 0
         k = m - 5
         if k % 8 == 0:
             k = int(k / 8)
         else:
             k = int(k / 8) + 1
-        pend_current = 0
+        pend_current = R[1]
         for i in range(1, k+1): 
-            r = ((1 - pend_current - r0)/ (2 * (1+math.sqrt(radius[i-1])))) ** 2
-            pend_current += 2 * math.sqrt(r * radius[i-1])
-            radius.append(r)
+            r = ((1 - pend_current)/ (2 * (1+math.sqrt(R[i-1])))) ** 2
+            pend_current += 2 * math.sqrt(r * R[i-1])
+            R.append(r)
             x = 1 - r
-            y = 1 - r0 - pend_current
+            y = 1 - R1 - pend_current
             for j in range(4):
-                circles.append(Circle((x * sym_x[j], y * sym_y[j]), radius[i]))
+                circles.append(Circle((x * sym_x[j], y * sym_y[j]), r))
                 if 8*(i-1)+j+5+1>8:
                     break
-                circles.append(Circle((y * sym_y[j], x * sym_x[j]), radius[i]))
+                circles.append(Circle((y * sym_y[j], x * sym_x[j]), r))
                 if 8*(i-1)+j+5>8:
                     break
         return circles
